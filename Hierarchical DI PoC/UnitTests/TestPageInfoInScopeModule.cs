cf48349674c9aa2422f5f3599fd5b05600cbbda4
iJungleboy <daniel.mettler@2sic.com>
2025-05-26 09:01:44 +0200

    moving pageinfo tests to own files

--- Hierarchical DI PoC/UnitTests/TestPageInfoInScopeModule.cs
+++ Hierarchical DI PoC/UnitTests/TestPageInfoInScopeModule.cs
@@ -7,16 +7,48 @@ public class TestPageInfoInScopeModule(IServiceProvider globalServiceProvider)
 {
     public const int MockPageId = 88;
 
-    [Fact]
-    public void TestTransientService()
+    private (IPageInfo ofPageScope, IPageInfo ofModuleScope) ArrangePageInfosOfPageAndModuleScope(int pageId, int moduleId)
     {
         // Arrange
         // Create a page scope and prepare shared page context
-        var pageSp = globalServiceProvider.SetupPage(MockPageId);
-
-        var pageOfPageScope = pageSp.GetRequiredService<IPageInfo>();
+        var pageSp = globalServiceProvider.SetupPage(pageId);
 
         // Simulate two modules on the same page, each with its own scope
+        var moduleSp1 = pageSp.SetupModule(moduleId);
+
+        // Act
+        var pageOfPageScope = pageSp.GetRequiredService<IPageInfo>();
+        var pageOfModuleScope = moduleSp1.GetRequiredService<IPageInfo>();
+        return (pageOfPageScope, pageOfModuleScope);
+    }
+
+    [Fact]
+    public void PageInfoHasSameIdInPageAndModuleScope()
+    {
+        var (pageOfPageScope, pageOfModuleScope1) = ArrangePageInfosOfPageAndModuleScope(21, 101);
+
+        // Assert all share the same pageId
+        Equal(21, pageOfPageScope.PageId);
+        Equal(pageOfPageScope.PageId, pageOfModuleScope1.PageId);
+    }
+
+    [Fact]
+    public void PageInfoFromPageAndModuleScopeAreNotSame()
+    {
+        var (pageOfPageScope, pageOfModuleScope1) = ArrangePageInfosOfPageAndModuleScope(21, 101);
+        // Assert that they are not the same instance
+        NotSame(pageOfPageScope, pageOfModuleScope1);
+    }
+
+    [Theory]
+    [InlineData(21)]
+    [InlineData(42)]
+    public void PageInfoFromMultipleModuleScopes(int pageId)
+    {
+        // Arrange
+        // Create a page scope and prepare shared page context
+        // Simulate two modules on the same page, each with its own scope
+        var pageSp = globalServiceProvider.SetupPage(pageId);
         var moduleSp1 = pageSp.SetupModule(101);
         var moduleSp2 = pageSp.SetupModule(102);
 
@@ -25,13 +57,10 @@ public class TestPageInfoInScopeModule(IServiceProvider globalServiceProvider)
         var pageOfModuleScope2 = moduleSp2.GetRequiredService<IPageInfo>();
 
         // Assert all share the same pageId
-        Equal(pageOfPageScope.PageId, pageOfModuleScope1.PageId);
-        Equal(pageOfPageScope.PageId, pageOfModuleScope2.PageId);
+        Equal(pageId, pageOfModuleScope1.PageId);
+        Equal(pageId, pageOfModuleScope2.PageId);
 
         // Assert that they are not the same instance
         NotSame(pageOfModuleScope1, pageOfModuleScope2);
-        NotSame(pageOfPageScope, pageOfModuleScope1);
-        NotSame(pageOfPageScope, pageOfModuleScope2);
-
     }
 }
